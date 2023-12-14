d228fd4975ba62307487a79c57a3dfd6b5fe758e
Qiang <30870950+QiangSu@users.noreply.github.com>
2023-12-14 15:06:21 +0800

    Add files via upload

--- pipeline_abundance_GaussF_esti_loop.py
+++ pipeline_abundance_GaussF_esti_loop.py
@@ -5,19 +5,23 @@ from scipy.optimize import curve_fit
 from scipy.stats import norm
 import os
 
+
 # Function to calculate the GC content percentage of a sequence
 def calculate_gc_content(seq):
     gc_count = seq.count('G') + seq.count('C')
     gc_content_percentage = (gc_count / len(seq)) * 100
     return gc_content_percentage
 
-# Function to calculate the Gaussian CDF
-def gaussian_cdf(x, A0, amplitude, mean, stddev):
-    return A0 + amplitude * norm.cdf(x, mean, stddev)
 
-# Function with fixed mean and stddev
-def gaussian_cdf_fixed(x, A0, amplitude, fixed_mean, fixed_stddev):
-    return gaussian_cdf(x, A0, amplitude, fixed_mean, fixed_stddev)
+# Adjusted Gaussian CDF function according to the specified equation
+def gaussian_cdf(x, A0, A, xc, w):
+    return A0 + A * norm.cdf((x - xc) / w)
+
+
+# Function to use the fixed mean and standard deviation for curve fitting
+def gaussian_cdf_fixed(x, A0, A, fixed_xc, fixed_w):
+    return gaussian_cdf(x, A0, A, fixed_xc, fixed_w)
+
 
 def main(input_folder, output_file_path):
     # Get the full path of the input folder
@@ -25,9 +29,6 @@ def main(input_folder, output_file_path):
 
     # Find all CSV files in the input folder
     csv_files = [file for file in os.listdir(input_folder_path) if file.endswith('.csv')]
-
-    # Initialize a dictionary to store amplitude results
-    amplitude_results = {}
 
     # Process each CSV file
     for file_name in csv_files:
@@ -47,65 +48,65 @@ def main(input_folder, output_file_path):
         # Drop NA values that could not be converted to float
         df.dropna(subset=['Frequency'], inplace=True)
 
-        # Add a new column to the dataframe for GC content
+        # Add a new column to the DataFrame for GC content
         df['GC Content (%)'] = df['K-mer'].apply(calculate_gc_content)
 
-        # Group by 'GC Content (%)' and sum up the 'Frequency' column
-        gc_content_grouped = df.groupby('GC Content (%)')['Frequency'].sum().reset_index()
+        # Group by 'GC Content (%)' and aggregate 'Frequency' column as sum and count
+        gc_content_grouped = df.groupby('GC Content (%)')['Frequency'].agg(['sum', 'size']).reset_index()
 
         # Sort the grouped data by 'GC Content (%)' in ascending order
         gc_content_grouped.sort_values('GC Content (%)', inplace=True)
 
-        # Calculate the cumulative frequency
-        gc_content_grouped['Accumulated Frequency'] = gc_content_grouped['Frequency'].cumsum()
+        # Calculate the accumulated 'Sum Accumulated' and 'Size Accumulated' values
+        gc_content_grouped['Sum Accumulated'] = gc_content_grouped['sum'].cumsum()
+        gc_content_grouped['Size Accumulated'] = gc_content_grouped['size'].cumsum()
 
-        # Perform the Gaussian CDF fit assuming fixed values for mean and stddev
-        initial_mean = np.mean(gc_content_grouped['GC Content (%)'])
-        initial_stddev = np.std(gc_content_grouped['GC Content (%)'])
+        # Fit the Gaussian CDF to the Size Accumulated data
+        initial_guess = [0, gc_content_grouped['Size Accumulated'].iloc[-1],
+                         gc_content_grouped['GC Content (%)'].mean(), gc_content_grouped['GC Content (%)'].std()]
+        popt, pcov = curve_fit(gaussian_cdf,
+                               gc_content_grouped['GC Content (%)'],
+                               gc_content_grouped['Size Accumulated'],
+                               p0=initial_guess)
+        A0_fit, A_fit, xc_fit, w_fit = popt
 
-        x_data_cdf = gc_content_grouped['GC Content (%)']
-        y_data_cdf = gc_content_grouped['Accumulated Frequency']
-
-        # Perform the Gaussian CDF fit using fixed values for mean and stddev
-        initial_guess_cdf = [1, max(y_data_cdf)]  # Baseline offset and amplitude
-
-        # Fitting the Gaussian CDF with fixed mean and stddev
-        popt_cdf, _ = curve_fit(
-            lambda x, A0, amplitude: gaussian_cdf_fixed(x, A0, amplitude, initial_mean, initial_stddev),
-            x_data_cdf,
-            y_data_cdf,
-            p0=initial_guess_cdf,
-            bounds=(0, [np.inf, np.inf])
+        # Use the fitted xc and w as fixed values to fit the Sum Accumulated data
+        initial_guess_sum = [0, gc_content_grouped['Sum Accumulated'].iloc[-1]]
+        popt_sum, pcov_sum = curve_fit(
+            lambda x, A0, A: gaussian_cdf_fixed(x, A0, A, xc_fit, w_fit),
+            gc_content_grouped['GC Content (%)'],
+            gc_content_grouped['Sum Accumulated'],
+            p0=initial_guess_sum
         )
+        A0_fit_sum, A_fit_sum = popt_sum
 
-        # Collect the amplitude_fit_cdf value
-        A0_fit_cdf, amplitude_fit_cdf = popt_cdf
-        amplitude_results[file_name] = amplitude_fit_cdf
+        # Format the output result
+        result_string = f"{os.path.splitext(file_name)[0]}: xc = {xc_fit}, w = {w_fit}, A = {A_fit_sum}\n"
 
-    # Write the amplitude_fit_cdf results for each file to the specified output file
-    with open(output_file_path, "w") as f:
-        for file_name, amplitude in amplitude_results.items():
-            f.write(f"{file_name}: {amplitude}\n")
+        # Write the result for this file to the output file
+        with open(output_file_path, "a") as f:
+            f.write(result_string)
 
-    print(f"Amplitude fit CDF results have been saved to {output_file_path}")
+    print(f"Results have been written to {output_file_path}")
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Analyze GC content and fit Gaussian CDF.")
     parser.add_argument(
         '--input',
         type=str,
-        default="unique_frequence_csv",
-        help="Path to the input folder containing the CSV files. Default is 'unique_frequence_csv'."
+        default="unique_frequency_csv",
+        help="Path to the input folder containing the CSV files. Default is 'unique_frequency_csv'."
     )
     parser.add_argument(
         '--output',
         type=str,
-        default="amplitude_fit_cdf_results.txt",
-        help="Path and name of the output file to save the results. Default is 'amplitude_fit_cdf_results.txt'."
+        default="results.txt",
+        help="Path and name of the output file to save the results. Default is 'results.txt'."
     )
     args = parser.parse_args()
 
-    # Check if the directory for the output file exists; if not, create it
+    # Ensure directory exists for the output file
     output_directory = os.path.dirname(os.path.abspath(args.output))
     if output_directory and not os.path.exists(output_directory):
         os.makedirs(output_directory)
@@ -113,5 +114,9 @@ if __name__ == "__main__":
     # Check if the input folder exists
     if not os.path.exists(args.input):
         raise FileNotFoundError(f"Input folder '{args.input}' not found.")
+
+    # Clear previous content before writing new results
+    if os.path.exists(args.output):
+        open(args.output, 'w').close()
 
     main(args.input, args.output)
